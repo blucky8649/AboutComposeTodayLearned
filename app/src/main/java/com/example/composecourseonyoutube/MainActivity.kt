ff61fbeb0189c3754ef99434ffb72e65bebed1cf
blucky8649 <blucky4511@gmail.com>
2022-02-16 17:02:34 +0900

    Styling Text

--- app/src/main/java/com/example/composecourseonyoutube/MainActivity.kt
+++ app/src/main/java/com/example/composecourseonyoutube/MainActivity.kt
@@ -22,7 +22,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,121 +40,51 @@ import com.example.composecourseonyoutube.ui.theme.ComposeCourseOnYouTubeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.roboto_thin, FontWeight.Thin),
+            Font(R.font.roboto_black, FontWeight.Black),
+            Font(R.font.roboto_bold, FontWeight.Bold),
+            Font(R.font.roboto_medium, FontWeight.Medium)
+        )
         setContent {
-            val painter = painterResource(id = R.drawable.kermit)
-            val description = "Kermit in the snow"
-            val title = "Kermit is playing in the snow"
-            Column(Modifier.background(Color.Cyan)) {
-                Row(
-                    Modifier
-                        .fillMaxWidth())
-                {
-                    Box(modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                        .padding(16.dp)
-                    ) {
-                        ImageCard(painter = painter,
-                            contentDescription = description,
-                            title = title)
-                    }
-
-                    Box(modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                    ) {
-                        ImageCard(painter = painter,
-                            contentDescription = description,
-                            title = title)
-                    }
-                }
-                Row(
-                    Modifier
-                        .fillMaxWidth())
-                {
-                    Box(modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                        .padding(16.dp)
-                    ) {
-                        ImageCard(painter = painter,
-                            contentDescription = description,
-                            title = title)
-                    }
-
-                    Box(modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                    ) {
-                        ImageCard(painter = painter,
-                            contentDescription = description,
-                            title = title)
-                    }
-                }
-                Row(
-                    Modifier
-                        .fillMaxWidth())
-                {
-                    Box(modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                        .padding(16.dp)
-                    ) {
-                        ImageCard(painter = painter,
-                            contentDescription = description,
-                            title = title)
-                    }
-
-                    Box(modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                    ) {
-                        ImageCard(painter = painter,
-                            contentDescription = description,
-                            title = title)
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 10.dp
-    ) {
-        Box(modifier = modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black,
-                        ),
-                        startY = 300f
-                    )
-                ))
             Box(
-                modifier = Modifier
+                Modifier
                     .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                    .background(Color(0xFF101010))) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("J")
+                        }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("C")
+                        }
+                        append("ompose")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    fontStyle = FontStyle.Italic,
+                    textDecoration = TextDecoration.Underline
+                )
             }
+
         }
     }
 }
+
+
+
